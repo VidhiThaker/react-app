41c232fc3d30844a225d82b6431c74a325b09aa7
vidhi <vidhithaker1909@gmail.com>
2020-10-24 23:12:35 -0400

    Updates

---
+++ precache-manifest.1718b45a1d33aa9c149ab8a5b4547c6f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b49e60d011df3d24fb9cbc731e50f4e3",
+    "url": "/react-app/index.html"
+  },
+  {
+    "revision": "5935a50d64feb70bfc10",
+    "url": "/react-app/static/css/main.d5c208e5.chunk.css"
+  },
+  {
+    "revision": "bd51d37fb58596b84fee",
+    "url": "/react-app/static/js/2.216a343a.chunk.js"
+  },
+  {
+    "revision": "f5fc2afae58c4ffc30c6148864765d4b",
+    "url": "/react-app/static/js/2.216a343a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5935a50d64feb70bfc10",
+    "url": "/react-app/static/js/main.a8fc99e0.chunk.js"
+  },
+  {
+    "revision": "605cf62042abf25f1d08",
+    "url": "/react-app/static/js/runtime-main.056c36fa.js"
+  },
+  {
+    "revision": "00f0bdb6e239452efab227524fd5df06",
+    "url": "/react-app/static/media/chefhero_logo_white.00f0bdb6.png"
+  }
+]);
